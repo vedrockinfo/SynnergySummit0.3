e78a2c22c8646f6bcc03e96ecce42d5fe9a5bc18
VedantCMS <vedantgupta.cms@gmail.com>
2024-05-21 14:42:48 +0530

    updates pushed

--- js/main.js
+++ js/main.js
@@ -97,14 +97,11 @@ jQuery('.rajnath-img').owlCarousel({
 
 $(document).ready(function () {
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
+        if ($(this).scrollTop() > 500) {
             $('#scroll-to-top').fadeIn();
         } else {
             $('#scroll-to-top').fadeOut();
         }
     });
-    $('#scroll-to-top').click(function () {
-        $('html, body').animate({ scrollTop: 0 }, 800);
-        return false;
-    });
+
 });
