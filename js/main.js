ae3d28b107bd4335186d14e06124d8aa3a9649df
VedantatCMS <vedantgupta.cms@gmail.com>
2024-06-24 11:20:32 +0530

    program updated

--- js/main.js
+++ js/main.js
@@ -20,6 +20,27 @@ $(".globalAdvisor").owlCarousel({
     },
 });
 
+$(".donor-agencies").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    navText: ['<i class="fa-solid fa-angle-left"></i>', '<i class="fa-solid fa-angle-right"></i>'],
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 2,
+        },
+        1000: {
+            items: 2,
+        },
+    },
+});
+
 jQuery('.latest-article').owlCarousel({
     loop: true,
     margin: 20,
